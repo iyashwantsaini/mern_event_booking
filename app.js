14d842dc8e28077270c92143ab8ebaedf573bf0c
meyash <yashsn2127@gmail.com>
2020-06-26 13:36:04 +0530

    merng

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const { buildSchema } = require('graphql');
 
 const app = express();
 
+const events = [];
+
 app.use(bodyParser.json()); // to parse incoming json bodies
 
 // all req are sent to /graphql
@@ -15,13 +17,29 @@ app.use('/graphql',
     // point to schema
     // pass props in schema
     // String! means type is string but can't be null
+    // every event must have an ID
     schema: buildSchema(`
+        type Event{
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        input EventInput{
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+    
         type RootQuery{
-            events: [String!]!
+            events: [Event!]!
         }
 
         type RootMutation{
-            createEvent(name: String): String
+            createEvent(eventInput: EventInput): Event
         }
         
         schema{
@@ -33,11 +51,18 @@ app.use('/graphql',
     rootValue: {
         // names of queries and resolvers are same
         events: () => {
-            return ['Romantic', 'Cooking', 'Swimming', 'Dancing'];
+            return events;
         },
         createEvent:(args)=>{
-            const eventName = args.name;
-            return eventName;
+            const event={
+                _id:Math.random().toString(),
+                title:args.eventInput.title,
+                description:args.eventInput.description,
+                price:+args.eventInput.price,
+                date:args.eventInput.date
+            };
+            events.push(event);
+            return event;
         }
     },
     graphiql:true
