51e97bcd64a5d84d9da456da97fdd0b4529d7b8a
meyash <yashsn2127@gmail.com>
2020-06-27 13:04:21 +0530

    merng

--- graphql/schema/index.js
+++ graphql/schema/index.js
@@ -10,6 +10,15 @@ const { buildSchema } = require("graphql");
 //  password should be nullable in type as we don't want to send it to any user
 
 module.exports = buildSchema(`
+
+type Booking{
+    _id: ID!
+    event: Event!
+    user: User!
+    createdAt: String!
+    updatedAt: String!
+}
+
 type Event{
     _id: ID!
     title: String!
@@ -40,11 +49,14 @@ input UserInput{
 
 type RootQuery{
     events: [Event!]!
+    bookings:[Booking!]!
 }
 
 type RootMutation{
     createEvent(eventInput: EventInput): Event
     createUser(userInput: UserInput) : User
+    bookEvent(eventId: ID!) : Booking!
+    cancelBooking(bookingId: ID!) : Event!
 }
 
 schema{
